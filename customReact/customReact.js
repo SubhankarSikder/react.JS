d148b37792a9d2c1931927f745d2ef90b130921a
Subhankar Sikder <subhankarsikder500@gmail.com>
2024-08-14 12:12:22 +0530

    creating custom react library

---
+++ customReact/customReact.js
@@ -0,0 +1,28 @@
+function customRender(reactElement, container){
+    // const domElement = document.createElement(reactElement.type)
+    // domElement.innerHTML = reactElement.children
+    // domElement.setAttribute('href', reactElement.props.href)
+    // domElement.setAttribute('target', reactElement.props.target)
+    // container.appendChild(domElement)
+
+    const domElement = document.createElement(reactElement.type)
+    domElement.textContent = reactElement.children
+    for (const key in reactElement.props) {
+       if(key == 'children') continue;
+       domElement.setAttribute(key, reactElement.props[key])
+    }
+    container.appendChild(domElement)
+}
+
+const reactElement = {
+    type: 'a',
+    props: {
+        href: 'https://google.com',
+        target: '_blank'
+    },
+    children: 'click me to visit google'
+}
+
+const mainContainer = document.getElementById('root')
+
+customRender(reactElement, mainContainer)
