0512cd164745ebd4e4c15defdec700d61ed97e41
Subhankar Sikder <subhankarsikder500@gmail.com>
2024-08-22 21:52:44 +0530

    project

--- 06currencyConverter/src/hooks/useCurrencyInfo.js
+++ 06currencyConverter/src/hooks/useCurrencyInfo.js
@@ -6,8 +6,11 @@ function useCurrencyInfo(currency){
     useEffect(() => {
         fetch(`https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies/${currency}.json`)
         .then((res) => res.json())
-        .then((res) => setData(res.currency))
+        .then((res) => setData(res[currency]))
         console.log(data);
 
     }, [currency])
+    return data
 }
+
+export default useCurrencyInfo
