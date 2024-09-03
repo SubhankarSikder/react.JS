23ba9e30f2bdac22b5f7e837ab1b765fd1448bfb
Subhankar Sikder <subhankarsikder500@gmail.com>
2024-09-03 20:17:47 +0530

    project

--- reduxToolkitTodo/src/features/todo/todoSlice.js
+++ reduxToolkitTodo/src/features/todo/todoSlice.js
@@ -13,7 +13,25 @@ export const todoSlice = createSlice({
     name: 'todo',
     initialState,
     reducers: {
-        addTodo: () => {},
-        removeTodo: () => {}
+        addTodo: (state, action) => {
+            const todo = {
+                id: nanoid(),
+                text: action.payload
+            }
+            state.todos.push(todo)
+        },
+        removeTodo: (state, action) => {
+            state.todos = state.todos.filter((todo) => todo.id !== action.payload)
+        },
+        updateTodo: (state, action) => {
+            const { userId, todoMsg } = action.payload;
+            const todo = state.todos.find((todo) => todo.id === userId);
+            if (todo) {
+                todo.text = todoMsg;
+            }
+        }
     }
 })
+
+export const {addTodo, removeTodo, updateTodo} = todoSlice.actions
+export default todoSlice.reducer
